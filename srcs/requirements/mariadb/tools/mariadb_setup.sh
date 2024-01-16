826b9e859fe52698ddc80ee8e0db75a56af1c927
sangyao <sangyao@sawang.sawang.42.de>
2024-01-16 21:19:33 +0100

    After restart vm, seems no connection prob with socket

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -19,25 +19,26 @@ ls -la /etc/mysql/mariadb.conf.d
 
 service mariadb start
 
-# mariadb << EOF
-# CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
-# CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-# GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-# FLUSH PRIVILEGES;
-# SHOW DATABASES;
-# EOF
-
 mariadb << EOF
-CREATE DATABASE IF NOT EXISTS test;
+CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 SHOW DATABASES;
 EOF
 
+# mariadb << EOF
+# CREATE DATABASE IF NOT EXISTS test;
+# FLUSH PRIVILEGES;
+# SHOW DATABASES;
+# EOF
+
 #kill $(cat /var/run/mysqld/mysqld.pid)
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
 
 echo "hi"
 
-exec mysqld_safe
+#exec mysqld_safe
+exec mariadbd-safe
