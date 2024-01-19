c06c9cdd21cc6dfe7b9f8e6ae1e8626e1e44485f
Sangyao <wangsangyao@hotmail.com>
2024-01-19 16:53:56 +0100

    modified[mariadb]: sleep before mariadb starts

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -8,22 +8,60 @@
 # DB_NAME="${MYSQL_DATABASE}"
 # DB_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}"
 
-service mariadb start
+if service mariadb start; then
+	 echo "INFO: Started MariaDB as a background service"
+else
+	 echo "ERROR: Failed to start MariaDB service."
+fi
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;\
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';\
-GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';\
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
+sleep 10
+#mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;\
+#CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';\
+#GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';\
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
+#FLUSH PRIVILEGES;\
+#SHOW DATABASES;"
+
+
+if mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;\
+CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';\
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';\
 FLUSH PRIVILEGES;\
-SHOW DATABASES;"
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
+FLUSH PRIVILEGES;"; then
+	 echo "INFO: MariaDB database created successfully"
+else
+	 echo "ERROR: Failed to create MariaDB database."
+fi
 
-#sleep 5
 
-#mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
+#if mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
+#FLUSH PRIVILEGES;";then
+#	 echo "INFO: MariaDB credentials set successfully"
+#else
+#	 echo "ERROR: Failed to set MariaDB credentials."
+#fi
 
-service mariadb stop
 
-exec mariadbd-safe
+echo $MYSQL_ROOT_PASSWORD;
+
+#mariadb-admin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+if mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown; then
+     echo "INFO: Stopped MariaDB as a background service"
+else
+     echo "ERROR: Failed to stop MariaDB service."
+fi
+
+#service mariadb stop
+
+#
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';\
+
+if exec mariadbd-safe; then
+	 echo "INFO: Started MariaDB as a foreground service"
+else
+	 echo "ERROR: Failed to start MariaDB service."
+fi
 
 
 
