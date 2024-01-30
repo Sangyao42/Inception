2688de3bef217b182f58f489bd4cd4f2911b6b60
sawang_vm <sawang@student.42heilbronn.de>
2024-01-30 18:09:00 +0100

    Fixed[nginx]: subsitute the variables in nginx.comf

--- srcs/requirements/nginx/tools/nginx_setup.sh
+++ srcs/requirements/nginx/tools/nginx_setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
 ln -sf /etc/nginx/sites-available/mynginx.conf /etc/nginx/sites-enabled/default
+
+MYCONFIG=/etc/nginx/sites-available/mynginx.conf
+
+envsubst '${CERTSN}' < $MYCONFIG > "$MYCONFIG.tmp" && mv "$MYCONFIG.tmp" "$MYCONFIG"
+envsubst '${DOMAIN_NAME}' < $MYCONFIG > "$MYCONFIG.tmp" && mv "$MYCONFIG.tmp" "$MYCONFIG"
+
+nginx -g "daemon off;"
