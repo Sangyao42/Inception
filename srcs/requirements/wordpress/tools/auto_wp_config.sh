13dec6efa772e068fd095e81e2e00ae0a51bb380
sawang_vm <sawang@student.42heilbronn.de>
2024-01-30 15:26:47 +0100

    Fixed[wp]: conflicts when using wp, instead using wp-cli.phar

--- srcs/requirements/wordpress/tools/auto_wp_config.sh
+++ srcs/requirements/wordpress/tools/auto_wp_config.sh
@@ -3,16 +3,13 @@
 #wait until mariabd starts
 sleep 10
 
-#wp core download --allow-root --path='/var/www/wordpress'
+wp-cli.phar core download --allow-root --path='/var/www/wordpress'
 
 #check if wp-config.php already exists
-if test -e "/var/www/wordpress/wp-config.php"; then
-	ls -la '/var/www/wordpress'
-	exit
-else
+if [ ! -e "/var/www/wordpress/wp-config.php" ]; then
 	#config the database
 	echo "create"
-	wp config create --allow-root --path='/var/www/wordpress' \
+	wp-cli.phar config create --allow-root --path='/var/www/wordpress' \
 		--dbname=$MYSQL_DATABASE \
 		--dbuser=$MYSQL_USER \
 		--dbpass=$MYSQL_PASSWORD \
@@ -20,16 +17,14 @@ else
 fi
 
 #configure the wordpress admin
-wp core install --url=$DOMAIN_NAME --title=$WP_TITLE \
+wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE \
 	--admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL \
-	--skip-email --allow-root
+	--skip-email --allow-root --path='/var/www/wordpress'
 
 #create a new user in wordpress
-wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+wp-cli.phar user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root --path='/var/www/wordpress'
 
-if test -e "/run/php"; then
-	exit
-else
+if [ ! -e "/run/php" ]; then
 	mkdir /run/php
 fi
 
